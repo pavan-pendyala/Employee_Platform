61ae56765cd9cf100dd1f76722ef8a400894ec1d
PavanKumarPendyala <pp5418@srmist.edu.in>
2026-05-27 12:49:34 +0530

    Adding observability

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 app = FastAPI()
 
+Instrumentator().instrument(app).expose(app)
 @app.get("/")
 def health():
     return {"status": "Healthy"}
